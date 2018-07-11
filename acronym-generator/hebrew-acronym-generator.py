cee826d54393e6f21a0e976f8e68967497402ee2
codingforpleasure <cohengil333@gmail.com>
2018-07-11 20:10:17 +0300

    acronym for Hebrew was added

---
+++ acronym-generator/hebrew-acronym-generator.py
@@ -0,0 +1,23 @@
+from __future__ import unicode_literals
+import HspellPy # Python wrapper for Hspell ssss    
+import itertools
+import sys
+
+if sys.version_info[0] < 3:
+    raise Exception("Python 3 or a more recent version is required.")
+
+speller = HspellPy.Hspell(linguistics=True)
+
+input = 'כנות צחוק חיבה גומות פיקניק שמש'
+words = input.split()
+tokens = [word[0].lower() for word in words]
+
+print("[INFO] - Generating acronyms")
+
+for i in range(1, len(tokens) + 1, 1):
+    permutations = itertools.permutations(tokens, i)
+    for permutation in permutations:
+        acronymn = ''.join(permutation) # Pay attention in case of hebrew the output in shell might be
+                                        #  presented in reverse order
+        if (speller.check_word(acronymn)): #acronymn
+            print("Found aronymn: " + acronymn)
